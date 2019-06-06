867c9112317cb165814c8ea97a2482f4eb848604
veropra <prainoveronica@gmail.com>
2019-06-06 13:55:13 +0200

    Milestone 1 e 2 ok...error charge1

--- script.js
+++ script.js
@@ -3,8 +3,35 @@ $(document).ready(function(){
   var url_base = 'http://157.230.17.132:4013/sales';
 
   stampa_chart();
+
+  $('.container').on('click', 'button', function() {
+      var impiegato = $('.nomi').val();
+      var data = $('.mesi').val();
+      var data_select = moment(data, 'YYYY-MM-DD').format('DD/MM/YYYY');
+
+      var new_val =  $('.importo').val();
+
+      input_dati(url_base, impiegato, data_select, new_val);
+      stampa_chart(url_base);
+    });
   //Creo una funzione che mi crea il grafico con i dati in get con una chiamata ajax
   function stampa_chart(){
+    $('.myChart1').html('');
+    $('.myChart2').html('');
+    var mesi = {
+      'January': 0,
+      'February': 0,
+      'March': 0,
+      'April': 0,
+      'May': 0,
+      'June': 0,
+      'July': 0,
+      'August': 0,
+      'September': 0,
+      'October': 0,
+      'November': 0,
+      'December': 0
+    }
 
     $.ajax({
       'url': url_base,
@@ -16,18 +43,23 @@ $(document).ready(function(){
         for (var i = 0; i < response.length; i++) {
           var data = response[i].date;
           var importo = response[i].amount;
-          var data_corrente_modif = data.slice(3, 5);
+          var data_changed = data.slice(3, 5);
           var mesi_trovati = Object.keys(vendite_mese);
 
-          if (!mesi_trovati.includes(data_corrente_modif)) {
-            vendite_mese[data_corrente_modif] = importo;
+          if (!mesi_trovati.includes(data_changed)) {
+            vendite_mese[data_changed] = importo;
           } else {
-            vendite_mese[data_corrente_modif] += importo;
+            vendite_mese[data_changed] += importo;
           }
         }
 
-        var label_mesi = Object.keys(vendite_mese);
-        var dati_mesi = Object.values(vendite_mese);
+        var label_mesi = Object.keys(mesi);
+        var dati_mesi = Object.values(mesi);
+
+        for (var mese in mesi) {
+          if (mese == moment_data)
+          mesi[mese] += importo;
+        }
 
         mese_singolo = [];
         for (var i = 0; i < label_mesi.length; i++) {
@@ -82,6 +114,18 @@ $(document).ready(function(){
           }
         }
 
+        //Appendo i nomi degli impiegati nella select
+        for (var i = 0; i < venditori_inseriti.length; i++) {
+          var impiegato = venditori_inseriti[i];
+          $('.nomi').append($('<option value="'+ impiegato +'">'+ impiegato +'</option>'));
+        }
+
+        //Appendo i mesi nella select
+        for (var i = 0; i < label_mesi.length; i++) {
+          var mese = label_mesi[i];
+          $('.mesi').append($('<option value="'+ mese +'">'+ mese +'</option>'));
+        }
+
         //Preparo le percentuali di vendita per ogni venditore
         for(var venditore in vendite){
           var totale_vendite = vendite[venditore];
@@ -127,4 +171,23 @@ $(document).ready(function(){
       }
     });
   }
+
+
+function input_dati(url, nome, giorno, valore) {
+  $.ajax({
+    'url': url,
+    'method': 'POST',
+    'data': {
+      'salesman': nome,
+      'date': giorno,
+      'amount': valore
+    },
+    'success': function (response) {
+
+    },
+    error: function () {
+      alert('errore');
+    }
+  })
+}
 });
